dd10e73eae1d7de0c748e1ea04c14e6e6aa357a6
ngyinkit <yinkit.ng.2021@scis.smu.edu.sg>
2023-07-19 14:32:07 +0800

    Update sqlite.py

--- test_scripts/sqlite.py
+++ test_scripts/sqlite.py
@@ -10,6 +10,7 @@ class Database():
         conn = None
         try:
             conn = sqlite3.connect(db_path)
+            conn.execute("PRAGMA foreign_keys = 1")
         except Error as e:
             print(e)
 
@@ -23,50 +24,142 @@ class Database():
             print(e)
 
     def main(db_path):
-        create_result_table = """ CREATE TABLE IF NOT EXISTS result (
-                                        SCAN_NO INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        PW_CRACKED TEXT NOT NULL,
-                                        FINAL_SCORE INTEGER NOT NULL,
-                                        DATE_TIME TEXT NOT NULL);"""
+        create_scan_table = """ CREATE TABLE IF NOT EXISTS scan (
+                                        scan_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        network_name TEXT NOT NULL,
+                                        date_time TEXT NOT NULL);"""
+        create_host_table = """ CREATE TABLE IF NOT EXISTS host (
+                                        host_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        scan_id INTEGER NOT NULL,
+                                        host_ip TEXT NOT NULL,
+                                        FOREIGN KEY (scan_id) REFERENCES scan (scan_id));"""
+        create_service_table = """ CREATE TABLE IF NOT EXISTS service (
+                                        service_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        host_id INTEGER NOT NULL,
+                                        service_name TEXT NOT NULL,
+                                        port_no INT NOT NULL,
+                                        pw_crakced BOOLEAN NOT NULL,
+                                        FOREIGN KEY (host_id) REFERENCES host (host_id));"""
+
         conn = Database.create_connection(db_path)
         if conn is not None:
-            Database.create_table(conn, create_result_table)
+            Database.create_table(conn, create_scan_table)
+            Database.create_table(conn, create_host_table)
+            Database.create_table(conn, create_service_table)
         else:
-            print("Error! cannot create the database connection.")
+            print("Error: cannot create the database connection.")
     
-    def get_results(db_path):
+    def get_all_scans(db_path):
         conn = Database.create_connection(db_path)
         try:
             cur = conn.cursor()
-            cur.execute("SELECT * FROM result")
+            cur.execute("SELECT * FROM scan")
             return cur
         except Error as e:
             print("Error: failed to get scan results")
             print(e)
     
-    def insert_result(db_path, pw_cracked, final_score):
+    def get_hosts(db_path, scan_no):
         conn = Database.create_connection(db_path)
         try:
             cur = conn.cursor()
-            cur.execute("INSERT INTO result (PW_CRACKED, FINAL_SCORE, DATE_TIME) VALUES (?, ?, datetime('now'))", (pw_cracked, final_score))
+            cur.execute("SELECT * FROM host WHERE scan_id = ?", [scan_no])
+            return cur
+        except Error as e:
+            print("Error: failed to get host results")
+            print(e)
+    
+    def get_services(db_path, host_no):
+        conn = Database.create_connection(db_path)
+        try:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM service WHERE host_id = ?", [host_no])
+            return cur
+        except Error as e:
+            print("Error: failed to get service results")
+            print(e)
+    
+    def insert_scan(db_path, network_name):
+        conn = Database.create_connection(db_path)
+        try:
+            cur = conn.cursor()
+            # insert into scan table
+            cur.execute("INSERT INTO scan (network_name, date_time) VALUES (?, datetime('now','localtime'))", [network_name])
+            conn.commit()
+            # get latest scan no
+            scan_id = cur.execute("SELECT MAX(scan_id) FROM scan").fetchone()[0]
+            # then check if scan_no is not none and insert into host table
+            # we need a for loop for this part for each host
+            host_ip = "192.168.0.108" # this is a placeholder for now
+            cur.execute("INSERT INTO host (scan_id, host_ip) VALUES (?, ?)", [scan_id, host_ip])
+            conn.commit()
+            # get latest host no
+            host_id = cur.execute("SELECT MAX(host_id) FROM host").fetchone()[0]
+            # then check if host_id is not none and insert into service table
+            # we need a for loop for this part for each service
+            # the vars below are placeholders for now
+            service_name = "ssh"
+            port_no = 22
+            pw_cracked = False
+            cur.execute("INSERT INTO service (host_id, service_name, port_no, pw_crakced) VALUES (?, ?, ?, ?)", [host_id, service_name, port_no, pw_cracked])
             conn.commit()
         except Error as e:
             print("Error: failed to insert scan result")
             print(e)
     
-    def delete_result(db_path, scan_no):
+    def delete_result(db_path, scan_id):
         conn = Database.create_connection(db_path)
         try:
             cur = conn.cursor()
-            cur.execute("DELETE FROM result WHERE SCAN_NO = ?", [scan_no])
+            # get corresponding list of host ids
+            host_ids = cur.execute("SELECT host_id FROM host WHERE scan_id = ?", [scan_id]).fetchall() # this returns a list of tuples
+            # delete all services with host_id in host_ids
+            for host_id in host_ids:
+                cur.execute("DELETE FROM service WHERE host_id = ?", [host_id[0]])
+            conn.commit()
+            # delete all hosts with scan_id
+            cur.execute("DELETE FROM host WHERE scan_id = ?", [scan_id])
+            cur.execute("DELETE FROM scan WHERE scan_id = ?", [scan_id])
             conn.commit()
         except Error as e:
             print("Error: failed to delete scan result")
             print(e)
+    
+    def get_all_hosts(db_path): # for testing purposes
+        conn = Database.create_connection(db_path)
+        try:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM host")
+            return cur
+        except Error as e:
+            print("Error: failed to get host results")
+            print(e)
+    
+    def get_all_services(db_path): # for testing purposes
+        conn = Database.create_connection(db_path)
+        try:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM service")
+            return cur
+        except Error as e:
+            print("Error: failed to get service results")
+            print(e)
 
-Database.main(r"results.db")
-Database.insert_result(r"results.db", "yes", 0)
-Database.delete_result(r"results.db", 1)
-final = Database.get_results(r"results.db") # final is a cursor object
+db_path = r"results.db"
+
+Database.main(db_path)
+Database.insert_scan(db_path, "wifi name")
+Database.insert_scan(db_path, "wifi name")
+Database.delete_result(db_path, 2)
+Database.insert_scan(db_path, "wifi name")
+final = Database.get_all_scans(db_path)
+final2 = Database.get_all_hosts(db_path)
+final3 = Database.get_all_services(db_path)
 for row in final:
+    print(row)
+print("-------------------")
+for row in final2:
+    print(row)
+print("-------------------")
+for row in final3:
     print(row)
