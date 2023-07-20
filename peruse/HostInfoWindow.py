93333478b3fb1a0fecac124f26d494aa8174dfb5
0venoven <51714567+0venoven@users.noreply.github.com>
2023-07-20 13:34:34 +0800

    Update sample data method for HostWindow

--- peruse/HostInfoWindow.py
+++ peruse/HostInfoWindow.py
@@ -23,3 +23,36 @@ class HostInfoWindow(QMainWindow):
         central_widget = QWidget(self)
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+
+    def add_sample_data(self, scan_id):
+        # This is just an example to populate the table widget, you'll replace this with data from your SQLite DB.
+        host_data = [
+            (f"{scan_id}-1", "Host 1 Info"),
+            (f"{scan_id}-2", "Host 2 Info"),
+        ]
+
+        for row, (host_id, host_info) in enumerate(host_data):
+            self.table_widget.insertRow(row)
+            self.table_widget.setItem(row, 0, QTableWidgetItem(host_id))
+            self.table_widget.setItem(row, 1, QTableWidgetItem(host_info))
+
+    def contextMenuEvent(self, event):
+        menu = QMenu(self)
+        selected_row = self.table_widget.currentRow()
+        if selected_row >= 0:
+            host_id = self.table_widget.item(selected_row, 0).text()
+            menu.addAction("View Services Information", lambda: self.show_services_info(host_id))
+
+        menu.exec(event.globalPos())
+
+    def show_services_info(self, host_id):
+        # Check if the services_window already exists for the selected host
+        for services_window in self.parent().services_windows:
+            if services_window.host_id == host_id:
+                services_window.show()
+                services_window.activateWindow()
+                break
+        else:
+            services_window = ServicesWindow(host_id, parent=self.parent())
+            self.parent().services_windows.append(services_window)
+            services_window.show()
